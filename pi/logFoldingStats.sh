155645be8152ac0b2d47bebfa795a214cc1b9a8f
Ubuntu <ubuntu@ubuntu.fritz.box>
2020-05-09 14:47:49 +0000

    Added WUS (working units) to folding.

---
+++ pi/logFoldingStats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+while true; 
+do 
+  CONTENT=$(curl -s -XGET https://stats.foldingathome.org/api/donor/bigheat4711)
+  CREDIT=$(echo $CONTENT | jq '.credit')
+  WUS=$(echo $CONTENT | jq '.wus')
+  echo "{ \"credit\": \"$CREDIT\", \"wus\": \"$WUS\"  }" | mosquitto_pub -l -t 'home/folding/stats'
+  sleep 900
+done
