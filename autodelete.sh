f3b18966eb19e9b5f9fd27674cf994ff89e3fed2
u5wsl <you@example.com>
2020-05-16 16:10:30 +0200

    autodel

--- autodelete.sh
+++ autodelete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+find ~/.autodelete/        -maxdepth 1 -mtime +30 -exec rm -rf '{}' \;
 
-find ~/.autodelete/        -maxdepth 1 -mtime +30 -exec rm -rf {} \;
 # might fail
 find ~/nethome/autodelete/ -maxdepth 1 -mtime +30 -exec rm -rf {} \; 2>/dev/null
